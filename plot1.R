fc4f2c66989b3f71bd217b13811593b5b6d25c56
srfnmnk <SupaflyYZ2003@msn.com>
2015-02-04 11:06:33 -0600

    Completed Plot1

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
 tmp <- read.delim("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE)
 powercon <- subset(tmp, as.Date(tmp$Date, "%d/%m/%Y") == "2007-02-01" | as.Date(tmp$Date, "%d/%m/%Y") == "2007-02-02")
 hist(as.numeric(powercon$Global_active_power), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+png("C:\\Dev\\git\\coursera\\ExData_Plotting1\\plot1.png", width = 480, height = 480, units = "px")
